3818f0c9dee31e955ddb33a3abd37cbab93fb063
Polymorph <team@polymorphtech.xyz>
2024-01-16 16:02:11 -0500

    complete app

---
+++ app/models/post.server.ts
@@ -0,0 +1,33 @@
+import { prisma } from "~/db.server";
+
+type Post = {
+    slug: string;
+    title: string;
+  };
+
+  export async function getPosts() {
+    return prisma.post.findMany();
+  }
+
+  /*
+  export async function getPosts(): Promise<Array<Post>> {
+    return [
+      {
+        slug: "my-first-post",
+        title: "My First Post",
+      },
+      {
+        slug: "90s-mixtape",
+        title: "A Mixtape I Made Just For You",
+      },
+    ];
+  }
+  */
+
+  export async function getPost(slug: string) {
+    return prisma.post.findUnique({ where: { slug } });
+  }
+
+  export async function createPost(post) {
+    return prisma.post.create({ data: post });
+  }
